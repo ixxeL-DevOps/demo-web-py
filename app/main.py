94308cae972be01a9aecea54219d444cc3c6cbe2
Frédéric SPIERS <fredspiers@gmail.com>
2024-09-08 15:25:15 +0200

    feat: new git write back2

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 # Définir une route pour gérer les requêtes GET sur '/'
 @app.get("/")
 async def read_root():
-    return {"message": "Bienvenue sur le serveur web new methode git write back SUCCESS!"}
+    return {"message": "Bienvenue sur le serveur web new methode git write back SUCCESS 2!"}
 
 if __name__ == "__main__":
     config = uvicorn.Config(app, port=8080, log_level="info")
